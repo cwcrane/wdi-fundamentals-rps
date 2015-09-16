28998704b2ea00b6fe171c203150a8023fc7470e
Christopher Crane <christophercrane@Christophers-MacBook-Pro.local>
2015-09-16 15:58:30 -0400

    added playtofive code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -63,5 +63,14 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    while (playerWins<5 || computerWins<5){
+        if (getWinner(playerMove,computerMove)=='player'){
+            playerWins +=1;
+        } else if (getWinner(playerMove,computerMove))=='computer'{
+            computerWins +=1;
+        }else ;
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
